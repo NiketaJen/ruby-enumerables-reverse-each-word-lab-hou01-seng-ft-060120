3b104ac47ae1b95e04f7c01605d2a8461d6a9015
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-19 03:27:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 
 def reverse_each_word (sentence)
   word_array = sentence.split(/ /) 
-  new_sentence = []
   
- word_array.each do |word|
-   rev_word = word.reverse
+ word_array.collect.reverse
  
   new_sentence << rev_word
 end
