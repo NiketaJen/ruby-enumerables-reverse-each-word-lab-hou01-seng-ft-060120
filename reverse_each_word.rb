d7466886aaa117607dc07b363bcdfbdb8a766ef1
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-19 02:31:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,14 +3,14 @@ def reverse_each_word (sentence)
   word_array = sentence.split
   new_sentence = []
   
- word_array.each {|word| rev_word = word.reverse}
+ word_array.each do |word|
+   rev_word = word.reverse
  
   puts rev_word
   new_sentence << rev_word
 end
 result = new_sentence.join(" ")
 result
-
 end
 
 
